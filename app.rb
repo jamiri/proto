a732657b11b79d6da3e2630aa7bf2190c98484e0
Hussein Samadi <hsamadi@iosie.net>
2012-08-06 14:08:47 +0430

    audio/video files moved to folder "assets". view_lesson_fake route removed. Old routes for "Category" removed. Some bugfixes. Some code cleaning.

--- app.rb
+++ app.rb
@@ -16,12 +16,11 @@ class Main < Sinatra::Base
   helpers Sinatra::ContentFor2
 
   map(:home).to('/')
-  map(:view_lesson_fake).to('/lesson')
   map(:feedback).to('/feedback')
   map(:view_lesson).to('/lesson/:id')
-  map(:view_audio).to('/files/audio/:file_name')
-  map(:view_video).to('/files/video/:file_name')
+
   set :ref_img_dir, 'assets/ref_img'
+  set :lesson_dir, 'assets/lesson_av'
 
   get :home do
 
@@ -49,71 +48,15 @@ class Main < Sinatra::Base
 
   # ----- Lesson -----
 
-  # Temporary address for viewing the "lesson" page
-  get :view_lesson_fake do
-    @categories = Category.where(:parent_id => nil)
-    erb :'lesson/index'
-  end
-
-
   get :view_lesson do
     #exception handling required!
 
     @lesson = Lesson.find(params[:id])
     @categories = Category.where(:parent_id => nil)
-    @questions=Question.where(:lesson_id=>1)
 
     erb :'lesson/index'
   end
 
-  #*************************************** Begin Audio Controller **************************
-
-  get :view_audio do
-
-
-    File.read(File.join("files/audio", @params['file_name'])) # Get the file path
-
-  end
-
-  #*************************************** End Audio Controller **************************
-
-
-
-#***************************** Category Controller *************************************
-
-  get "/category/add/?" do
-    erb :'category/create'
-  end
-
-
-#---------------------------------------------------------------------------------------------
-
-
-  post "/category/add/?" do
-    category = params[:category]
-
-    c = Category.new
-    c.name = category[:name]
-    c.parent = Category.where(:name => category[:parent]).first
-    c.save
-
-
-    erb :"category/new", :locals => {:cat => c}
-  end
-
-#***************************** End of Category Controller ******************************
-
-
-
-
-#***************************** Lesson Controller *************************************
-
-
-
-
-#***************************** End of Lesson Controller ***********************************
-
   use SalaamPodAdmin
-  #use LessonController
 
 end
