3e491ac931c73d12108d652452280b6bfe4f9f7c
javad <jamiri@iosie.net>
2012-08-08 18:00:23 +0430

    finished audio podcast

---
+++ public/js/podcast.js
@@ -0,0 +1,17 @@
+// Commented by Hussein Samadi
+//
+$(document).ready(function () {
+    $("#jquery_jplayer_1").jPlayer({
+        ready:function () {
+
+
+            var audio_path = document.getElementById("Podcast").getAttribute("audio_path");
+
+            $(this).jPlayer("setMedia", {
+                mp3: audio_path
+            });
+        },
+        swfPath:"/js",
+        supplied:"mp3"
+    });
+});
