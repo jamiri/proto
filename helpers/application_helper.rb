f0cb7d19f6251a7759c67d15f0679c1ffd992385
javad <jamiri@iosie.net>
2012-08-29 12:35:09 +0430

    Some code cleaning was performed

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -53,16 +53,15 @@ def create_navbar(id)
   # the string that is going to be returned
   str = ""
 
-  cat=Category.find(id)
-  str= "<li><a href='#'>"<< cat.name<< "</a></li>"+str
+  cat = Category.find(id)
+  str = "<li><a href='#'>" << cat.name << "</a></li>" + str
   if cat.parent_id.nil?
 
     str
 
   else
 
-    str= create_navbar(cat.parent_id)+str
-    str
+   create_navbar(cat.parent_id) + str
 
   end
 end
