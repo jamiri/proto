681f4bba5cab8fe27f7eb63d529bd550a2fac722
meqdadh <meqdadh@gmail.com>
2012-08-29 09:38:42 +0430

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	views/lesson/partial/_questions.erb

--- db/models/BlogPost.rb
+++ db/models/BlogPost.rb
@@ -3,4 +3,9 @@ class BlogPost < ActiveRecord::Base
   belongs_to :lessons
   has_many :comments, :foreign_key => 'blog_post_id'
 
+  def posted_on
+    #TODO: decision: created_at or updated_at?
+    created_at
+  end
+
 end
